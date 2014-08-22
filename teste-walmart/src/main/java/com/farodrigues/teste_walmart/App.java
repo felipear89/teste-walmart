bd446af8c35f7862dc0d91c7436939878245aecf
Felipe Alexandre Rodrigues <felipe@farodrigues.com>
2014-08-22 00:21:32 -0300

    Primeiro commit

---
+++ teste-walmart/src/main/java/com/farodrigues/teste_walmart/App.java
@@ -0,0 +1,13 @@
+package com.farodrigues.teste_walmart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
