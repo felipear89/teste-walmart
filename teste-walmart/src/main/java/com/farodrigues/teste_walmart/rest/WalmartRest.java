2cad416e5551177fd4afb7be3191be75494278ed
Felipe Alexandre Rodrigues <felipe@farodrigues.com>
2014-08-27 23:31:50 -0300

    Alterando controller de @Controller para @RestController

--- teste-walmart/src/main/java/com/farodrigues/teste_walmart/rest/WalmartRest.java
+++ teste-walmart/src/main/java/com/farodrigues/teste_walmart/rest/WalmartRest.java
@@ -9,18 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.farodrigues.teste_walmart.calc.CalculaCustoViagem;
 import com.farodrigues.teste_walmart.domain.Mapa;
 import com.farodrigues.teste_walmart.domain.Rota;
 import com.farodrigues.teste_walmart.service.MapaService;
 
-@Controller
+@RestController
 public class WalmartRest {
 
 	@Autowired
